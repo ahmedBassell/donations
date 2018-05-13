5fd9f21444999bfdbda1b52267573e3c6ac001ed
DevHossamHassan <hossamhassancs15@gmail.com>
2018-05-13 13:08:55 +0200

    Update Total donations to 3100

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@
 // Add values to the values array and see what happens :)
 var TotalCount = 0;
 var Sunday,Monday,Tuesday,Wednesday;
-Sunday = 1800;
+Sunday = 3200;
 Monday = 0;
 Tuesday = 0;
 Wednesday = 0;
